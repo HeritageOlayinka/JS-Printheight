168241e7b6b3b1b8f7630bd83c97b6ca63edb0e2
XpertHeritage <105379174+HeritageOlayinka@users.noreply.github.com>
2022-06-24 14:37:18 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+const myName = "Heritage Olayinka";
+const myHeight = "186 cm";
+const myCountry = "Nigeria";
+
+alert("My name is " + myName+ "\nMy height is " + myHeight + "\nMy country is " + myCountry)
